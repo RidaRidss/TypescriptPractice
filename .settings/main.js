b768d907635bdf90ee1a776ca1fab493635d8c43
RidaRidss <ridarocks12@yahoo.com>
2015-05-19 16:28:16 +0500

    commit

---
+++ .settings/main.js
@@ -0,0 +1,31 @@
+var x = "Hello World";
+document.write(x);
+/*  var obj={ name : "Rida" , id : 1 }
+obj={ name : "Rida" , id : 1 , remarks : "80%" }   */
+/*above code samw as */
+/*var obj :any ={ name : "Rida" , id : 1 };
+obj= { remarks : "80%" } ;
+obj={module: "B"};
+
+*/
+/*
+var a:number =1;
+var b :string = a.toString();
+var compare : boolean = (1==1);
+var c : number = 1 + 2;
+
+*/
+/* var myString : string ="Pakistan";
+var myNumber : number =1;
+var myBoolean : boolean =true;
+var a : number = null;
+
+var a=1;
+var b="1"
+if(a.toString() === b){
+    console.log("true");
+}
+else{
+    console.log("false");
+}
+*/ 
