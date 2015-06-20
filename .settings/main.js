da9ed82df5dfc82f20b59d6187bd8978f7b8daa7
RidaSarwarBilgrami <ridarocks12@yahoo.com>
2015-06-20 23:40:05 +0500

    Update main.js

--- .settings/main.js
+++ .settings/main.js
@@ -301,6 +301,7 @@ console.log(list['0']);
 //alert(arrayOfNumbers[1]);
 //5. enum
 // an enum is a way of giving more friendly names to sets of numeric values
+//TypeScript will assign a numeric value to each of these human readable enum values.
 //enum Color {Red,Green,Blue};
 // if we have not already set the value in enum , it will set the values itself .
 // By default, enums begin numbering their members starting at 0.
@@ -309,6 +310,28 @@ console.log(list['0']);
 //console.log(c); //value 1 will be loged on console
 // or we can search the values like this
 //var d : Color = Color["1"];
+//enum Color {Red, Green, Blue};//starts with 0
+//var c: Color = Color.Green;
+//
+//enum Color1 {Red = 1, Green, Blue};
+//var colorName: string = Color[2];
+//console.log(colorName);
+//
+//enum Color2 {Red = 1, Green = 2, Blue = 4};//can assign values to all
+//var colorIndex = Color2["Blue"];
+//console.log(colorIndex);
+// const enum
+//const enum Color {Red, Green, Blue};//starts with 0
+//var c: Color = Color.Green;
+//
+//const enum Color1 {Red = 1, Green, Blue};
+////var colorName: string = Color[2] //Not allowed with const enums
+//console.log(colorName);
+//
+//
+//const enum Color2 {Red = 1, Green = 2, Blue = 4};//can assign values to all
+//var colorIndex = Color2["Blue"];
+//console.log(colorIndex);
 //6. Any
 // The 'any' type is a powerful way to work with existing JavaScript, allowing you to gradually opt-in and opt-out of type-checking during compilation.
 //let myType : any = { name: "Zia", id: 1 };
@@ -344,4 +367,135 @@ console.log(list['0']);
 //let myType = { name: "Zia", id: 1 };
 //myType = { id: 2,  name: "Tom" };// can only assign a type which has the atleast the same properties
 //myType = { id: 3,  name: "Mike", gender: false };//can add a property
+//function
+//function add(x: number, y: number): number {//Named function
+//	return x+y;
+//}
+//
+//var myAdd1 = function(x: number, y: number): number { //Anonymous function
+//	return x+y;
+//};
+//
+//var myAdd2: (x:number, y:number)=>number = 	function(x: number, y: number): number { //Anonymous function with type
+//	return x+y;
+//};
+//
+//var myAdd3: (baseValue:number, increment:number)=>number = 	function(x: number, y: number): number { //type names dont matter
+//	return x+y;
+//};
+//Optional parameters
+//var concatStrings = function(a, b, c) {
+//	return a + b + c;
+//}
+//console.log(concatStrings("a", "b", "c"));
+//console.log(concatStrings("a", "b"));  // this line will be loged as abundefined i.e concatenated string( a , b , undefined)
+// or //Error: Supplied parameters do not match any signature of call target.
+//TypeScript introduces the question mark ? syntax to indicate optional parameters.
+//function buildName(firstName: string, lastName?: string) : string {//Named function with optional parameters
+//	if (lastName)
+//		return firstName + " " + lastName;
+//	else
+//		return firstName;
+//}
+//console.log( buildName("Bob"));  //works correctly because last parameter is optional
+////var result2 = buildName("Bob", "Adams", "Sr.");  //error, too many parameters
+//var result4=buildName("Bob", "Adams");  //correct
+////anonymous function type with optional parameters
+//var buildName1 : (firstName: string, lastName?: string) => string =
+//	function(firstName: string, lastName?: string) : string {
+//		if (lastName)
+//			return firstName + " " + lastName;
+//		else
+//			return firstName;
+//	}
+//note : The optional parameters must be the last parameters in the function definition. You can have as many optional parameters as you want, as long as non-optional parameters precede the optional parameters.
+//function buildName(firstName: string, lastName = "Smith") {
+//	return firstName + " " + lastName;
+//}
+//
+//var result1 = buildName("Bob");  //works correctly now, also
+//alert(result1);
+//var result2 = buildName("Bob", "Adams", "Sr.");  //error, too many parameters
+//alert(result2);
+//var result3 = buildName("Bob", "Adams");  //ah, just right
+//alert(result3);
+//note : share the same type "(firstName: string, lastName?: string)=>string". The default value of the default parameter disappears, leaving only the knowledge that the parameter is optional.
+//Default parameters
+// Note that using the default parameter syntax will automatically make the parameter optional.
+//function buildName(firstName: string, lastName = "Khan") : string {//Named function with optional parameters
+//	if (lastName)
+//		return firstName + " " + lastName;
+//	else
+//		return firstName;
+//}
+//
+//var result1 = buildName("Bob");  //works correctly because last parameter is optional
+//alert(result1);
+////var result2 = buildName("Bob", "Adams", "Sr.");  //error, too many parameters
+//var result3 = buildName("Bob", "Adams");  //correct
+//
+////anonymous function type with defult parameters (Note that the parameter type will be optional even with defult value)
+//var buildName1 : (firstName: string, lastName?: string) => string =
+//	function(firstName: string, lastName = "Khan") : string {
+//		if (lastName)
+//			return firstName + " " + lastName;
+//		else
+//			return firstName;
+//	}
+//Rest Parameters
+// Rest parameters can be used in any function be it function/()=>/class member.
+//you may not know how many parameters a function will ultimately take. In JavaScript, you can work with the arguments direction using the arguments variable that is visible inside every function body
+//function buildName(firstName: string, ...restOfName: string[]) {
+//	return firstName + " " + restOfName.join(" ");
+//}
+//
+//var employeeName = buildName("Joseph", "Samuel", "Lucas", "MacKinzie");
+//function buildName(firstName: string, ...restOfName: string[]) {//Named function with Rest parameters
+//	return firstName + " " + restOfName.join(" ");
+//}
+//
+//var employeeName = buildName("Joseph", "Samuel", "Lucas", "MacKinzie");
+//
+//
+//
+//
+////anonymous function type with Rest parameters
+//var buildNameFun: (fname: string, ...rest: string[])=>string =
+//	function (firstName: string, ...restOfName: string[]) {
+//		return firstName + " " + restOfName.join(" ");
+//	}
+/* lambda expression  or fat arrow function
+//You don't need to keep typing function
+//It lexically captures the meaning of this
+ var add = (a: number, b: number) => {
+ return a + b;
+ }
+
+ var add1 = (x: number, y: number) => (x + y);
+
+ var myFunction = f => { this.x = "x"; }//automatically creating the that-equals-this pattern
+above line of code same execute like this
+ var _this = this;
+ var myFunction = function (f) {
+ _this.x = "x";
+ };
+*/
+// Call back functions
+//A callback function is a function that is passed into another function
+//       function overloads
+//overloads give us type-checked calls
+//its customary to order overloads from most specific to least specific
+//function add(arg1: string, arg2: string): string;//option 1
+//function add(arg1: number, arg2: number): number;//option 2
+//function add(arg1: boolean, arg2: boolean): boolean;//option 3
+//function add(arg1: any, arg2: any): any {//this is not part of the overload list, so it only has three overloads
+//	return arg1 + arg2;
+//}
+//
+////Calling 'add' with any other parameter types would cause an error except for the three options
+//console.log(add(1, 2)); //3
+//console.log(add("Hello", "World"));//HelloWorld
+//console.log(add(true, false)); //true
+//console.log(add(true, 2));  //3
+//console.log(add(1,false));  //1
 //# sourceMappingURL=main.js.map
